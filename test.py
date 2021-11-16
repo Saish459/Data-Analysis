540dd84d9e15ed757f402692c5d8ae5ccafcd367
Pavan Saish <89860317+saish2003@users.noreply.github.com>
2021-11-16 11:18:37 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cv2
-
-img = cv2.imread('food.jpg')
-img = cv2.line(img, (0,0), (255,255), (0,0,255), 10 )
-cv2.imshow('Image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
